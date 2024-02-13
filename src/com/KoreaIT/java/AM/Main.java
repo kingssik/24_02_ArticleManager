106478f11931d2b2ea5eb13a4e073c2be878e92c
kingssik <hyunsikkang25@gmail.com>
2024-02-13 11:42:19 +0900

    게시글 목록 기능 구현중

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -1,5 +1,7 @@
 package com.KoreaIT.java.AM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,9 +10,11 @@ public class Main {
     Scanner sc = new Scanner(System.in);
 
     int lastArticleId = 0;
+    List<Article> articles = new ArrayList<>();
+
     while (true) {
       System.out.print("명령어 ) ");
-      String cmd = sc.nextLine();
+      String cmd = sc.nextLine().trim();
 
       if (cmd.length() == 0) {
         System.out.println("명령어를 입력하세요.");
@@ -28,11 +32,19 @@ public class Main {
         System.out.print("내용 : ");
         String body = sc.nextLine();
 
+        Article article = new Article(id, title, body);
+        articles.add(article);
+
         System.out.printf("%d번 글이 생성되었습니다.\n", id);
         lastArticleId = id;
 
       } else if (cmd.equals("article list")) {
-        System.out.println("게시글이 없습니다.");
+        if (articles.size() == 0) {
+          System.out.println("게시글이 없습니다.");
+          continue;
+        } else {
+          System.out.println("게시글이 있습니다.");
+        }
       } else {
         System.out.println("존재하지 않는 명령어입니다.");
       }
@@ -40,5 +52,17 @@ public class Main {
 
     sc.close();
     System.out.println("== 프로그램 종료 ==");
+  }
+}
+
+class Article {
+  int id;
+  String title;
+  String body;
+
+  public Article(int id, String title, String body) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
   }
 }
