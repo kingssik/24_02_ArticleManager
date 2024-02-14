15b9d3982cbaeb3d4e407d879b8251d253fbcb51
kingssik <hyunsikkang25@gmail.com>
2024-02-14 10:34:04 +0900

    게시물 상세 페이지 구현(날짜 아직)

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -50,6 +50,30 @@ public class Main {
           }
         }
 
+      } else if (cmd.startsWith("article detail ")) {
+        String[] cmdBits = cmd.split(" ");
+        int id = Integer.parseInt(cmdBits[2]);
+
+        Article foundArticle = null;  // 찾은 게시글을 연결하는 변수 foundArticle (가정)
+
+        for (int i = 0; i < articles.size(); i++) { // 게시글의 존재 여부 확인하는 과정
+          Article article = articles.get(i);  // 일일이 비교하기 위해 가져옴
+          if (article.id == id) { // articles에서 가져온 article 객체의 글번호와 id 비교
+            foundArticle = article;
+            break;
+          }
+        }
+
+        if (foundArticle == null) {
+          System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+          continue;
+        } else {
+          System.out.printf("번호 : %d\n", foundArticle.id);
+          System.out.printf("날짜 : %s\n", "2024-02-14 12:12:12");
+          System.out.printf("제목 : %s\n", foundArticle.title);
+          System.out.printf("내용 : %s\n", foundArticle.body);
+        }
+
       } else {
         System.out.println("존재하지 않는 명령어입니다.");
       }
