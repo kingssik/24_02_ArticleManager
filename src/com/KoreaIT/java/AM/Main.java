27487716f95a9107a0ffcea38773c28636b4466c
kingssik <hyunsikkang25@gmail.com>
2024-02-15 10:11:31 +0900

    게시물 수정 기능 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -97,6 +97,34 @@ public class Main {
           System.out.printf("%d번 게시물이 삭제 되었습니다.\n", id);
         }
 
+      } else if (cmd.startsWith("article modify ")) {
+        String[] cmdBits = cmd.split(" ");
+        int id = Integer.parseInt(cmdBits[2]);
+
+        Article foundArticle = null;
+
+        for (int i = 0; i < articles.size(); i++) {
+          Article article = articles.get(i);
+          if (article.id == id) {
+            foundArticle = article;
+            break;
+          }
+        }
+
+        if (foundArticle == null) {
+          System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+          continue;
+        } else {
+          System.out.print("제목 : ");
+          String title = sc.nextLine();
+          System.out.print("내용 : ");
+          String body = sc.nextLine();
+
+          foundArticle.title = title;
+          foundArticle.body = body;
+          System.out.printf("%d번 게시물이 수정 되었습니다.\n", id);
+        }
+
       } else {
         System.out.println("존재하지 않는 명령어입니다.");
       }
